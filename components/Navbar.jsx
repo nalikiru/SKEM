88e260ce341f7c65abb23eef7427c8cf6b6402db
kiru <90724833+nalikiru@users.noreply.github.com>
2023-02-24 12:30:59 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">EKVERSE Book</Link>
+        <Link href="/">EKVERSE Store</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
